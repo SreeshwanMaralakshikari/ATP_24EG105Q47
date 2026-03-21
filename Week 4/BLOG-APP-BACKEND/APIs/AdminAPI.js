021aa1a2fae99b3dce379d4beef4b7ce8f61fd22
SreeshwanMaralakshikari <24eg105q47@anurag.edu.in>
2026-03-21 19:30:05 +0530

    commit for week 5 assignment

--- Week 4/BLOG-APP-BACKEND/APIs/AdminAPI.js
+++ Week 4/BLOG-APP-BACKEND/APIs/AdminAPI.js
@@ -1,4 +1,9 @@
 import exp from 'express';
+import {UserModel} from '../models/UserModel.js';
+import {hash,compare} from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import {config} from 'dotenv';
+import { verifyToken } from '../middlewares/VerifyToken.js';
 
 export const adminApp=exp.Router();
 
